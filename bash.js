0d7e85ad6511cb5adccbe20271f3c48d50429996
Amy Tran <trantm.amy@gmail.com>
2021-03-05 12:46:28 -0800

    added ls.js

--- bash.js
+++ bash.js
@@ -1,16 +1,19 @@
 // output prompt 
-process.stdout.write('prompt > ');
+process.stdout.write('promptMain > ');
 
 const pwd = require('./pwd.js');
+const ls = require('./ls.js');
 
 
 // stdin 'data' event fires when user types in line
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
     if (cmd === 'pwd') {
-        pwd();
+      pwd();
+    } else if (cmd === 'ls') {
+      ls();
     } else {
         process.stdout.write('You typed: ' + cmd);
     }
-    process.stdout.write('\nprompt > ');
+    process.stdout.write('\npromptEXTRA > ');
 })
